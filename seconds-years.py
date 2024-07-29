308064cfb8dc55327d6eca7a4802a9487c4a42d5
AWannaB <theocarruth+github@gmail.com>
2024-07-29 15:28:21 -0700

    Update
    Updated to to catch error when a letter is entered

--- seconds-years.py
+++ seconds-years.py
@@ -1,15 +1,24 @@
 a = True
 while a is True:
-    seconds = int(input('Seconds:'))
-    years = seconds // 31536000
 
-    days = seconds % 31536000 // 86400
+    seconds = (input('Seconds:'))
+    try:
+        int(seconds) // 2
+    except ValueError:
+        if seconds == 'q':
+            a = False
+            continue
+        print('Not a valid number.')
+    else:
+        years = int(seconds) // 31536000
 
-    hours = seconds % 31536000 % 86400 // 3600
+        days = int(seconds) % 31536000 // 86400
 
-    minutes = seconds % 31536000 % 86400 % 3600 // 60
+        hours = int(seconds) % 31536000 % 86400 // 3600
 
-    final_seconds = seconds % 31536000 % 86400 % 3600 % 60 % 60
+        minutes = int(seconds) % 31536000 % 86400 % 3600 // 60
 
-    print(f'{seconds} seconds is the same as')
-    print(f'{years} years, {days} days, {hours} hours, {minutes} minutes, and {final_seconds} seconds')
+        final_seconds = int(seconds) % 31536000 % 86400 % 3600 % 60 % 60
+
+        print(f'{seconds} seconds is the same as')
+        print(f'{years} years, {days} days, {hours} hours, {minutes} minutes, and {final_seconds} seconds')
